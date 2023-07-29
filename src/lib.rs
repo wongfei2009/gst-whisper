305c3a2436bff96d4b24504e81cf22428797d9fb
wongfei2009 <wongfei2009@gmail.com>
2023-07-29 16:59:12 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,18 @@ use gstreamer::glib;
 mod filter;
 
 fn plugin_init(plugin: &gstreamer::Plugin) -> Result<(), glib::BoolError> {
-  filter::register(plugin)?;
-  Ok(())
+    filter::register(plugin)?;
+    Ok(())
 }
 
 gstreamer::plugin_define!(
-  whisper,
-  env!("CARGO_PKG_DESCRIPTION"),
-  plugin_init,
-  concat!(env!("CARGO_PKG_VERSION"), "-", env!("COMMIT_ID")),
-  "MIT/Apache-2.0",
-  env!("CARGO_PKG_NAME"),
-  env!("CARGO_PKG_NAME"),
-  env!("CARGO_PKG_REPOSITORY"),
-  env!("BUILD_REL_DATE")
+    whisper,
+    env!("CARGO_PKG_DESCRIPTION"),
+    plugin_init,
+    concat!(env!("CARGO_PKG_VERSION"), "-", env!("COMMIT_ID")),
+    "MIT/Apache-2.0",
+    env!("CARGO_PKG_NAME"),
+    env!("CARGO_PKG_NAME"),
+    env!("CARGO_PKG_REPOSITORY"),
+    env!("BUILD_REL_DATE")
 );
