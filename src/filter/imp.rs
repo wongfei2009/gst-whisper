d229ef54904648645d5d1ea9ee3290605ff76d44
wongfei2009 <wongfei2009@gmail.com>
2023-07-31 17:28:26 +0800

    Update imp.rs

--- src/filter/imp.rs
+++ src/filter/imp.rs
@@ -2,7 +2,7 @@ use std::{
     env,
     sync::{
         atomic::{AtomicBool, Ordering},
-        mpsc, Arc, Mutex,
+        mpsc, Arc, Condvar, Mutex,
     },
     thread,
     time::Instant,
@@ -403,33 +403,51 @@ impl WhisperFilter {
 struct VoiceActivityDetector {
     vad_sender: mpsc::Sender<Vec<i16>>,
     voice_activity_detected: Arc<AtomicBool>,
+    result_ready: Arc<(Mutex<bool>, Condvar)>,
 }
 
 impl VoiceActivityDetector {
     pub fn new(vad_mode: VadMode) -> Self {
         let voice_activity_detected = Arc::new(AtomicBool::new(false));
+        let result_ready = Arc::new((Mutex::new(false), Condvar::new()));
         let (vad_sender, vad_receiver) = mpsc::channel::<Vec<i16>>();
-        {
-            let voice_activity_detected = voice_activity_detected.clone();
-            thread::spawn(move || {
-                let mut vad =
-                    Vad::new_with_rate_and_mode((SAMPLE_RATE as i32).try_into().unwrap(), vad_mode);
-                while let Ok(next) = vad_receiver.recv() {
-                    let result = vad.is_voice_segment(&next).unwrap();
-                    voice_activity_detected.store(result, Ordering::Relaxed);
-                }
-            })
-        };
+        let voice_activity_detected_clone = voice_activity_detected.clone();
+        let result_ready_clone = result_ready.clone();
+
+        thread::spawn(move || {
+            let mut vad =
+                Vad::new_with_rate_and_mode((SAMPLE_RATE as i32).try_into().unwrap(), vad_mode);
+            while let Ok(next) = vad_receiver.recv() {
+                let result = vad.is_voice_segment(&next).unwrap();
+
+                let (lock, cvar) = &*result_ready_clone;
+                let mut result_ready = lock.lock().unwrap();
+                *result_ready = true;
+                voice_activity_detected_clone.store(result, Ordering::Relaxed);
+                cvar.notify_one();
+            }
+        });
 
         Self {
             vad_sender,
             voice_activity_detected,
+            result_ready,
         }
     }
 
     pub fn is_voice_segment(&self, buffer: &[i16]) -> Result<bool, ()> {
         self.vad_sender.send(buffer.to_vec()).unwrap();
-        Ok(self.voice_activity_detected.load(Ordering::Relaxed))
+
+        let (lock, cvar) = &*self.result_ready;
+        let mut result_ready = lock.lock().unwrap();
+        while !*result_ready {
+            result_ready = cvar.wait(result_ready).unwrap();
+        }
+        let result = self.voice_activity_detected.load(Ordering::Relaxed);
+        *result_ready = false;
+        self.voice_activity_detected.store(false, Ordering::Relaxed);
+
+        Ok(result)
     }
 }
 
