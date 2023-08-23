8f71e3c863591f84e60b8025d3a793eecdef7ec3
wongfei2009 <wongfei2009@gmail.com>
2023-08-23 12:14:35 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,22 @@ use gstreamer::glib;
 
 mod filter;
 
+/// Initializes the plugin by registering the filter.
+///
+/// # Arguments
+///
+/// * `plugin` - A reference to the GStreamer plugin.
+///
+/// # Returns
+///
+/// Returns `Ok(())` if the filter is successfully registered, otherwise returns a `glib::BoolError`.
 fn plugin_init(plugin: &gstreamer::Plugin) -> Result<(), glib::BoolError> {
     filter::register(plugin)?;
     Ok(())
 }
 
+// This macro defines a GStreamer plugin named "whisper" with the given metadata.
+// The metadata includes the plugin's description, initialization function, version, license, name, origin, repository, and build date.
 gstreamer::plugin_define!(
     whisper,
     env!("CARGO_PKG_DESCRIPTION"),
