a782ded9deabd7d0063957bc6c2c95bfa5d4182d
wongfei2009 <wongfei2009@gmail.com>
2023-08-23 12:21:29 +0800

    Update filter.rs

--- src/filter.rs
+++ src/filter.rs
@@ -2,15 +2,25 @@ mod imp;
 
 use gstreamer::{glib, prelude::StaticType, Rank};
 
+// A GStreamer filter element that applies "whisper" AI to an audio stream.
 glib::wrapper! {
   pub struct WhisperFilter(ObjectSubclass<imp::WhisperFilter>) @extends gstreamer_base::BaseTransform, gstreamer::Element, gstreamer::Object;
 }
 
+/// Registers the whisper filter plugin with GStreamer.
+///
+/// # Arguments
+///
+/// * `plugin` - A reference to the GStreamer plugin.
+///
+/// # Returns
+///
+/// * `Result<(), glib::BoolError>` - A result indicating success or failure.
 pub fn register(plugin: &gstreamer::Plugin) -> Result<(), glib::BoolError> {
-    gstreamer::Element::register(
-        Some(plugin),
-        "whisper",
-        Rank::None,
-        WhisperFilter::static_type(),
-    )
+  gstreamer::Element::register(
+    Some(plugin),
+    "whisper",
+    Rank::None,
+    WhisperFilter::static_type(),
+  )
 }
