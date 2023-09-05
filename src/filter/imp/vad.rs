939b56432079261997abcf83856dd522ace64e59
wongfei2009 <wongfei2009@gmail.com>
2023-09-05 10:05:06 +0800

    Update vad.rs

--- src/filter/imp/vad.rs
+++ src/filter/imp/vad.rs
@@ -18,13 +18,18 @@ use std::sync::Arc;
 
 use std::sync::mpsc;
 
+/// Voice Activity Detector struct used for detecting voice activity in audio streams.
 pub(crate) struct VoiceActivityDetector {
+    /// Sender channel for sending audio samples to the VAD.
     pub(crate) vad_sender: mpsc::Sender<Vec<i16>>,
+    /// Atomic boolean flag indicating whether voice activity has been detected.
     pub(crate) voice_activity_detected: Arc<AtomicBool>,
+    /// Mutex and Condvar pair used for synchronizing the result of the VAD.
     pub(crate) result_ready: Arc<(Mutex<bool>, Condvar)>,
 }
 
 impl VoiceActivityDetector {
+    /// Creates a new instance of `Vad` with the specified `vad_mode`.
     pub fn new(vad_mode: VadMode) -> Self {
         let voice_activity_detected = Arc::new(AtomicBool::new(false));
         let result_ready = Arc::new((Mutex::new(false), Condvar::new()));
@@ -53,6 +58,17 @@ impl VoiceActivityDetector {
         }
     }
 
+    /// Determines whether the given buffer contains a voice segment or not using Voice Activity Detection (VAD).
+    ///
+    /// # Arguments
+    ///
+    /// * `buffer` - An array slice of 16-bit signed integers representing the audio buffer to be analyzed.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(true)` if the buffer contains a voice segment.
+    /// * `Ok(false)` if the buffer does not contain a voice segment.
+    /// * `Err(())` if there was an error sending the buffer to the VAD sender.
     pub fn is_voice_segment(&self, buffer: &[i16]) -> Result<bool, ()> {
         self.vad_sender.send(buffer.to_vec()).unwrap();
 
